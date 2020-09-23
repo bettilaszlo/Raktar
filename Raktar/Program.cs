65995d0de6da3f52218947350814f6b7f1e03c84
bettilaszlo <laszlobetti3@gmail.com>
2020-09-23 14:56:03 +0200

    Beolvas elkezdése

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Raktar
 {
+
     class Program
     {
+        static void BeolvasRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+
+            while (!raktar.EndOfStream)
+            {
+                string[] sor = raktar.ReadLine().Split(';');
+               // Console.WriteLine(sor[0]);
+            }
+            raktar.Close();
+        }
         static void Main(string[] args)
         {
+            Termek t = new Termek("P01", "Póló XXL", 1500, 10);
+            BeolvasRaktar();
+
+            Console.WriteLine($"{t.Kod} - {t.Nev} - {t.Ar} - {t.DB}");
+            Console.ReadKey();
         }
     }
 }
---
+++ Raktar/Termek.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Raktar
+{
+    class Termek
+    {
+        public string kod;
+        public string Kod
+        {
+            get { return kod; }
+            set { kod = value; }
+        }
+
+        public string nev;
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        public int ar;
+        public int Ar
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
+        public int db;
+        public int DB
+        {
+            get { return db; }
+            set { db = value; }
+        }
+        public Termek(string kod, string nev, int ar, int db)
+        {
+            this.kod =kod;
+            this.nev = nev;
+            this.ar = ar;
+            this.db = db;
+
+        }
+       
+    }
+}
