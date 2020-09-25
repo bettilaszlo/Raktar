83412d883160c30e7357bd6826aeaa179994010f
bettilaszlo <laszlobetti3@gmail.com>
2020-09-25 09:04:23 +0200

    Megrendelés osztály tételek nélkül

---
+++ Raktar/Megrendeles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raktar
+{
+    class Megrendeles
+    {
+        public string datum;
+        public string Datum 
+        {
+            get { return datum; }
+            set { datum = value; }
+        }
+
+        public string megrendelo;
+        public string Megrendelo
+        {
+            get { return megrendelo; }
+            set { megrendelo = value; }
+        }
+
+        public string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public Megrendeles(string datum, string megrendelo, string email)
+        {
+            this.datum = datum;
+            this.megrendelo = megrendelo;
+            this.email = email;
+
+        }
+
+    }
+}
--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -11,6 +11,7 @@ namespace Raktar
     class Program
     {
         static List<Termek> termekek = new List<Termek>();
+        static List<Megrendeles> rendelesek = new List<Megrendeles>();
         static void BeolvasRaktar()
         {
             StreamReader raktar = new StreamReader("raktar.csv");
@@ -23,14 +24,39 @@ namespace Raktar
             }
             raktar.Close();
         }
+        static void BeolvasRendelesek()
+        {
+            StreamReader rendeles = new StreamReader("rendeles.csv");
+
+            while (!rendeles.EndOfStream)
+            {
+                string[] sor = rendeles.ReadLine().Split(';');
+
+                if (sor[0] == "M")
+                {
+                    rendelesek.Add(new Megrendeles(sor[1], sor[2], sor[3]));
+
+                }
+                // Console.WriteLine(sor[0]);
+            }
+            rendeles.Close();
+
+        }
         static void Main(string[] args)
         {
             BeolvasRaktar();
+            BeolvasRendelesek();
+
             foreach (var t in termekek)
             {
                 Console.WriteLine(t.Nev);
             }
 
+
+            foreach (var r in rendelesek)
+            {
+                Console.WriteLine(r.Email);
+            }
             //Termek t = new Termek("P01", "Póló XXL", 1500, 10);
             //Console.WriteLine($"{t.Kod} - {t.Nev} - {t.Ar} - {t.DB}");
             Console.ReadKey();
